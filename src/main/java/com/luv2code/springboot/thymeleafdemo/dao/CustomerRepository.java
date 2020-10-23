4f0203f72ac1d9357eb326b8cfbe2b8668cc4710
bxchang04 <bxchang04@gmail.com>
2020-10-23 14:38:33 -0400

    implement customers pages

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/dao/CustomerRepository.java
@@ -0,0 +1,18 @@
+package com.luv2code.springboot.thymeleafdemo.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+	
+	// add a method to sort by last name
+	public List<Customer> findAllByOrderByLastNameAsc();
+	
+	// search by name
+	public List<Customer> findByFirstNameContainsOrLastNameContainsAllIgnoreCase(String name, String lName);
+
+}

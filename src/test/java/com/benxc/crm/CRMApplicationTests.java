c49b8711c846748c2400e244514f370c6df3c87e
bxchang04 <bxchang04@gmail.com>
2020-10-26 17:32:02 -0400

    rewrite unit tests

--- src/test/java/com/benxc/crm/CRMApplicationTests.java
+++ src/test/java/com/benxc/crm/CRMApplicationTests.java
@@ -1,55 +1,84 @@
 package com.benxc.crm;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import com.benxc.crm.dao.UserDaoImpl;
-import com.benxc.crm.dao.UserDao;
+import com.benxc.crm.controller.CustomerController;
+import com.benxc.crm.controller.EmployeeController;
+import com.benxc.crm.dao.CustomerRepository;
+import com.benxc.crm.dao.EmployeeRepository;
+import com.benxc.crm.entity.Customer;
+import com.benxc.crm.entity.Employee;
 import com.benxc.crm.entity.Users;
-import com.benxc.crm.service.UserService;
-import com.benxc.crm.user.CrmUser;
+import com.benxc.crm.service.CustomerService;
+import com.benxc.crm.service.EmployeeService;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class CRMApplicationTests {
 
 	@Autowired
-	private UserService service;
+	private EmployeeService service;
+
+	@Autowired
+	private CustomerService cservice;
 	
 	@MockBean
-	private UserDao userDao;
+	private EmployeeRepository repository;
+
+	@MockBean
+	private CustomerRepository crepository;
 	
 	@Test
-	public void findByUserName() {
-		String name="hhill";
-		when(userDao.findByUserName(name)).thenReturn(new Users("hhill", "arst", 1, "Hank", "Hill", "hhill@hh.com"));
-		assertEquals(name, service.findByUserName(name).getUserName());
+	public void findEmployeesTest() {
+		when(repository.findAll()).thenReturn(Stream
+				.of(new Employee(100, "Hank", "Hill", "hhill@hh.com"), new Employee(101, "Hank2", "Hill2", "hhill2@hh.com")).collect(Collectors.toList()));
+		assertEquals(2, repository.findAll().size());
+	}
 
+	@Test
+	public void findEmployeeByIdTest() {
+		Optional<Employee> newEmp = Optional.of(new Employee(100, "Hank", "Hill", "hhill@hh.com"));
+		
+		when(repository.findById(100)).thenReturn(newEmp);
+		assertEquals(newEmp, repository.findById(100));
 	}
 	
-	// Refactor this
 //	@Test
-//	public void save() {
-//		CrmUser user=new CrmUser();
-//		doNothing().when(service.save(user)).thenReturn(user);
-//		assertArrayEquals(user, service.save(user));
-//
+//	public void DeleteEmployeeByIdTest() {
+//		Optional<Employee> newEmp = Optional.of(new Employee(100, "Hank", "Hill", "hhill@hh.com"));
+//		
+//		when(repository.deleteById(100).doNothing());
+//		assertEquals(true, repository.findById(100));
 //	}
+	
+	@Test
+	public void findCustomersTest() {
+		when(crepository.findAll()).thenReturn(Stream
+				.of(new Customer(100, "Harry", "Hill", "hhill@hh.com"), new Customer(101, "Harry2", "Hill2", "hhill2@hh.com")).collect(Collectors.toList()));
+		assertEquals(2, crepository.findAll().size());
+	}
+	
+	@Test
+	public void findCustomerByIdTest() {
+		Optional<Customer> newCust = Optional.of(new Customer(100, "Harry", "Hill", "hhill@hh.com"));
+		
+		when(crepository.findById(100)).thenReturn(newCust);
+		assertEquals(newCust, crepository.findById(100));
+	}
+
 
 	
 }
--- src/test/java/com/benxc/crm/dao/EmployeeRepositoryTest.java
+++ src/test/java/com/benxc/crm/dao/EmployeeRepositoryTest.java
@@ -1,70 +1,70 @@
-package com.benxc.crm.dao;
-
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import com.benxc.crm.dao.RoleDao;
-import com.benxc.crm.entity.Employee;
-import com.benxc.crm.entity.Role;
-import com.benxc.crm.entity.Users;
-import com.benxc.crm.service.UserService;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.mockito.stubbing.OngoingStubbing;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
-
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class EmployeeRepositoryTest {
-	
-	@MockBean
-	private EmployeeRepository repo;
-	
-	@SuppressWarnings("unchecked")
-	@Test
-	// add a method to sort by last name
-	public void findAllByOrderByLastNameAsc(){
-		String name = "hank";
-		String lname = "hill";
-		Employee test = new Employee(1, name, lname, "hhill@gmail.com");
-		when(((OngoingStubbing<List<Employee>>) repo.findAllByOrderByLastNameAsc()).thenReturn((List<Employee>) test));
-		assertSame((List<Employee>)test, repo.findAllByOrderByLastNameAsc());
-
-	};
-	
-	@SuppressWarnings("unchecked")
-	@Test
-	// search by name
-
-	public void findByFirstNameContainsOrLastNameContainsAllIgnoreCase(){
-		String name = "hank";
-		String lname = "hill";
-		Employee test = new Employee(1, name, lname, "hhill@gmail.com");
-		when(repo.findByFirstNameContainsOrLastNameContainsAllIgnoreCase(name, lname)).thenReturn((List<Employee>) test);
-		assertSame((List<Employee>)test, repo.findByFirstNameContainsOrLastNameContainsAllIgnoreCase(name, lname));
-
-	};
-
-}
+//package com.benxc.crm.dao;
+//
+//import static org.junit.Assert.assertArrayEquals;
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertSame;
+//import static org.junit.jupiter.api.Assertions.assertAll;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertNotEquals;
+//import static org.mockito.Mockito.mock;
+//import static org.mockito.Mockito.when;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//import com.benxc.crm.dao.RoleDao;
+//import com.benxc.crm.entity.Employee;
+//import com.benxc.crm.entity.Role;
+//import com.benxc.crm.entity.Users;
+//import com.benxc.crm.service.UserService;
+//
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Tag;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.junit.runner.RunWith;
+//import org.mockito.Mockito;
+//import org.mockito.stubbing.OngoingStubbing;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class EmployeeRepositoryTest {
+//	
+//	@MockBean
+//	private EmployeeRepository repo;
+//	
+//	@SuppressWarnings("unchecked")
+//	@Test
+//	// add a method to sort by last name
+//	public void findAllByOrderByLastNameAsc(){
+//		String name = "hank";
+//		String lname = "hill";
+//		Employee test = new Employee(1, name, lname, "hhill@gmail.com");
+//		when(((OngoingStubbing<List<Employee>>) repo.findAllByOrderByLastNameAsc()).thenReturn((List<Employee>) test));
+//		assertSame((List<Employee>)test, repo.findAllByOrderByLastNameAsc());
+//
+//	};
+//	
+//	@SuppressWarnings("unchecked")
+//	@Test
+//	// search by name
+//
+//	public void findByFirstNameContainsOrLastNameContainsAllIgnoreCase(){
+//		String name = "hank";
+//		String lname = "hill";
+//		Employee test = new Employee(1, name, lname, "hhill@gmail.com");
+//		when(repo.findByFirstNameContainsOrLastNameContainsAllIgnoreCase(name, lname)).thenReturn((List<Employee>) test);
+//		assertSame((List<Employee>)test, repo.findByFirstNameContainsOrLastNameContainsAllIgnoreCase(name, lname));
+//
+//	};
+//
+//}
